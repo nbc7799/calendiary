0af43e952ccaf35e6ebe2d9635e66dad772559c2
nbc7799 <nbc7799@naver.com>
2022-01-24 17:10:37 +0900

    login styles 구현

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -6,18 +6,37 @@ const Login = (props) => {
     <h1 className={styles.display}>
       <div className={styles.loginBox}>
         <div className={styles.title}>Login</div>
-        <div className={styles.input}>
-          <input type="text" placeholder="Email adress" />
-          <input type="text" placeholder="password" />
+        <div className={styles.inputBox}>
+          <div className={styles.idBox}>
+            <input type="text" placeholder="Email adress" />
+          </div>
+          <div className={styles.passBox}>
+            <input type="text" placeholder="password" />
+            <button className={styles.look} type="button">
+              <i class="far fa-eye"></i>
+            </button>
+            <button className={styles.hiden}>
+              <i class="far fa-eye-slash"></i>
+            </button>
+          </div>
         </div>
         <div className={styles.check}>
-          <input type="checkbox"></input>
-          <p>forgot password?</p>
+          <div className={styles.remember}>
+            <input type="checkbox" />
+            <p>remember me</p>
+          </div>
+          <input
+            className={styles.forgotPass}
+            type="button"
+            value="forgot password?"
+          />
         </div>
-        <button className={styles.login}>LOGIN</button>
+        <button type="button" className={styles.login}>
+          LOGIN
+        </button>
         <div className={styles.goSignup}>
-          <p>Don't have an account?</p>
-          <p>Create</p>
+          <p>Don't have an account? </p>
+          <input className={styles.create} type="button" value="Create" />
         </div>
       </div>
     </h1>
