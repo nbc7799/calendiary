41df1f65d159e8faacf167ec1765cb8b8a311722
nbc7799 <nbc7799@naver.com>
2022-01-26 23:44:05 +0900

    캘린더 메인페이지

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./main.module.css";
 
-const Main = (props) => <div>heelo</div>;
+const Main = (props) => {
+  let DATE = new Date();
+  const MONTH = DATE.getMonth() + 1;
+  const YEAR = DATE.getFullYear();
+  const date = DATE.getDate();
+  const day = DATE.getDay();
+
+  const [month, setMonth] = useState(MONTH);
+  const [yaer, setYear] = useState(YEAR);
+
+  const lastMonth = () => {
+    setMonth(month - 1);
+    console.log(month);
+  };
+  const preMonth = () => {
+    setMonth(month + 1);
+    console.log(month);
+  };
+
+  return (
+    <div className={styles.header}>
+      <div>
+        <button onClick={lastMonth}>&lt;</button>
+        <button onClick={preMonth}>&gt;</button>
+      </div>
+      <div className={styles.now}>
+        <div>{`${yaer}년`}</div>
+        <div>{`${month}월`}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
